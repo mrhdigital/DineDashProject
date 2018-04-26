87b1cf0f06dacb3aee9173a8f8d3f77560163302
Gavin Peterson <gavinpeterson@er-10-129-19-112.wireless.umn.edu>
2018-04-26 18:31:49 -0500

    changing file structure

--- assets/javascript/ingredientSearch.js
+++ assets/javascript/ingredientSearch.js
@@ -1,4 +1,5 @@
   // Initialize Firebase
+  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyC9y55fnvNPqajMd9zcWttEMcvM86rjnuE",
     authDomain: "nutrition-project-b321a.firebaseapp.com",
@@ -7,17 +8,25 @@
     storageBucket: "nutrition-project-b321a.appspot.com",
     messagingSenderId: "986829728187"
   };
+
   firebase.initializeApp(config);
+  
+var database = firebase.database();
 
-database = database.firebase();
 
-var searchInput;
+function createSearchQuery() {
+  
+}
 
-var queryURL = 'https://api.edamam.com/search?app_id=64622731&app_key=720fb1becfca77bf78494a9ce7272cc6&q=' + searchInput;
+var queryURL = 'https://api.edamam.com/search?app_id=64622731&app_key=720fb1becfca77bf78494a9ce7272cc6&q='+ searchInput;
 
 $.ajax({
     url: queryURL,
     method: 'GET'
 }).then(function(response){
+  console.log(response);
 
+  for (var i = 0; i < response.hits.length; i++) {
+
+  }
 })
