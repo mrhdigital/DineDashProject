706a901219bc764a5927fc7a42451cece1040c84
Gavin Peterson <gavinpeterson@er-10-129-19-112.wireless.umn.edu>
2018-04-26 20:31:49 -0500

    added search functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,33 +3,4 @@ $(document).ready(function () {
 
   var instance = M.Modal.getInstance($(".modal"));
   instance.open();
-
-
-
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC9y55fnvNPqajMd9zcWttEMcvM86rjnuE",
-    authDomain: "nutrition-project-b321a.firebaseapp.com",
-    databaseURL: "https://nutrition-project-b321a.firebaseio.com",
-    projectId: "nutrition-project-b321a",
-    storageBucket: "nutrition-project-b321a.appspot.com",
-    messagingSenderId: "986829728187"
-  };
-  firebase.initializeApp(config);
-
-  database = database.firebase();
-
-  var searchInput;
-
-  var queryURL = 'https://api.edamam.com/search?app_id=64622731&app_key=720fb1becfca77bf78494a9ce7272cc6&q=' + searchInput;
-
-  $.ajax({
-    url: queryURL,
-    method: 'GET'
-  }).then(function (response) {
-
-  })
-
-
-});
-
+})
